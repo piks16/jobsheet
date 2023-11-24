dc0688b325b9b5244668a7893fb8be57366b61b7
Hasanul Fikri <147679280+piks16@users.noreply.github.com>
2023-11-24 19:40:39 +0700

    Add files via upload

---
+++ jobsheet 1/praktikum 1.cpp
@@ -0,0 +1,13 @@
+//dibawah ini adalah bagian header yang berfungsi mengimpor fungsi pada header
+#include<stdio.h>
+
+//dibawah ini adalah bagian fungsi utama yang akan dieksekusi oleh IDE
+int main(){;
+	
+// dibawah ini printf("") adalah fungsi untuk menampilkan teks pada konsol. 
+// Fungsi ini akan menampilkan semua teks yang berada dalam kurung
+	printf("hello World"); 
+	
+// dibawah ini adalah return 0 yang menandakan bahwa fungsi telah berakhir
+	return 0;
+}
